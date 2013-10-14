11af8b1947af990fefc93c0db0355e75503205d8
ivanbrennan <ivan.brennan@gmail.com>
2013-10-14 10:41:48 -0400

    Add my-student-scraper.rb with initial instructions.

---
+++ my-student-scraper.rb
@@ -0,0 +1,11 @@
+=begin
+Your goal is to get a database that has all the student data (so say if we wanted to build a command line version of the student website we could use that database).
+   
+  So you need to create a table or something to store all the student's data.
+   
+  Each student will probably be a row, with columns for their attributes.
+   
+  Then you go to students.flatironschool.com and via nokogiri, find all the individual profile pages. Open each one of those (probably in a loop) and from the individual students page, scrape all their data.
+   
+  Once you have all the students, drop it into a database.
+=end

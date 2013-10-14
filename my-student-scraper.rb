0de092fc8d32065f28bd5ba09f932791816d846f
ivanbrennan <ivan.brennan@gmail.com>
2013-10-14 11:12:28 -0400

    Create open a database and create a simple table.

--- my-student-scraper.rb
+++ my-student-scraper.rb
@@ -1,11 +1,32 @@
 =begin
 Your goal is to get a database that has all the student data (so say if we wanted to build a command line version of the student website we could use that database).
    
-  So you need to create a table or something to store all the student's data.
+So you need to create a table or something to store all the student's data.
    
-  Each student will probably be a row, with columns for their attributes.
+Each student will probably be a row, with columns for their attributes.
    
-  Then you go to students.flatironschool.com and via nokogiri, find all the individual profile pages. Open each one of those (probably in a loop) and from the individual students page, scrape all their data.
+Then you go to students.flatironschool.com and via nokogiri, find all the individual profile pages. Open each one of those (probably in a loop) and from the individual students page, scrape all their data.
    
-  Once you have all the students, drop it into a database.
+Once you have all the students, drop it into a database.
 =end
+
+require 'sqlite3'
+require 'open-uri'
+require 'nokogiri'
+
+# Open a database
+db = SQLite3::Database.new "students.db"
+
+# Create a table in the database
+sql_create_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+
+db.execute(sql_create_table)
+
+index = "http://students.flatironschool.com/"
+
+html = Nokogiri::HTML(open(index))

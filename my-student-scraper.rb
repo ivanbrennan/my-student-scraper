5d06a9a8729c2f00c13acf2d39e0db7bbe012768
ivanbrennan <ivan.brennan@gmail.com>
2013-10-14 12:25:01 -0400

    Collect array of Nokogiri html objects for profiles.

--- my-student-scraper.rb
+++ my-student-scraper.rb
@@ -1,10 +1,5 @@
 =begin
 Your goal is to get a database that has all the student data (so say if we wanted to build a command line version of the student website we could use that database).
-   
-So you need to create a table or something to store all the student's data.
-   
-Each student will probably be a row, with columns for their attributes.
-   
 Then you go to students.flatironschool.com and via nokogiri, find all the individual profile pages. Open each one of those (probably in a loop) and from the individual students page, scrape all their data.
    
 Once you have all the students, drop it into a database.
@@ -14,6 +9,7 @@ IVAN: do hard-coded, then try to add abstractions
 require 'sqlite3'
 require 'open-uri'
 require 'nokogiri'
+require 'ap'
 
 # Open a database
 db = SQLite3::Database.new "students.db"
@@ -34,4 +30,8 @@ index_html = Nokogiri::HTML(open(flatiron_domain))
 index_links = index_html.css("div.big-comment a")
 index_urls = index_links.collect do |link|
   "#{flatiron_domain}#{link["href"]}"
+end
+
+profiles_html_arr = index_urls.collect do |url|
+  profile_html = Nokogiri::HTML(open(url))
 end
